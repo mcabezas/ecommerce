b21c8e71897e0a7c96212e4f3ab53c94fcac0caa
mcabezas <mcabezas@outlook.com>
2020-10-25 15:40:55 -0300

    Discount
      * Add combo discount calculation
      * Add unit tests

--- checkout/models/discount.go
+++ checkout/models/discount.go
@@ -25,7 +25,17 @@ func NewComboDiscount(watchID WatchID, units int, fixedPrice float64) ComboDisco
 	}
 }
 func (ud *ComboDiscount) CalculateDiscount(purchaseOrder PurchaseOrder) (money.Money, error) {
-	return money.Money{Amount: 0}, nil
+	if ud.units == 0 {
+		return money.Money{Amount: 0}, nil
+	}
+	originalPrice := float64(purchaseOrder.Items[ud.watchID].Qty) * purchaseOrder.Items[ud.watchID].UnitPrice
+
+	combos := purchaseOrder.Items[ud.watchID].Qty / ud.units
+	outOfCombos := purchaseOrder.Items[ud.watchID].Qty % ud.units
+	discountedPrice := float64(combos)*ud.fixedPrice +
+		float64(outOfCombos)*purchaseOrder.Items[ud.watchID].UnitPrice
+
+	return money.Money{Amount: originalPrice - discountedPrice}, nil
 }
 
 func (ud *ComboDiscount) Name() string {
--- checkout/usecases/findDiscounts_test.go
+++ checkout/usecases/findDiscounts_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/mcabezas/ecommerce/checkout/infrastructure"
 	"github.com/mcabezas/ecommerce/checkout/models"
+	"github.com/mcabezas/ecommerce/internal/money"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -42,6 +43,93 @@ func TestCanFindMultipleUnitDiscount(t *testing.T) {
 	)
 	assert.Nil(t, err)
 	assert.True(t, len(discounts) == 2)
+}
+
+func TestCanCalculateZeroDiscount(t *testing.T) {
+	// given
+	finder := buildDiscountFinder([]models.ComboDiscount{
+		models.NewComboDiscount("001", 3, 200),
+		models.NewComboDiscount("002", 3, 120),
+		models.NewComboDiscount("003", 0, 120),
+	})
+	watchIDs := []models.WatchID{"002", "003"}
+	watchIDsSet := removeDuplicatedIDs(watchIDs)
+	discounts, err := finder.findComboDiscounts(watchIDsSet)
+
+	purchaseOrder := watchIDsToPurchaseOrder(watchIDs, map[models.WatchID]models.WatchCatalogueItem{
+		"002": {ID: "002", Name: "Random2", UnitPrice: 80},
+		"003": {ID: "003", Name: "Random3", UnitPrice: 50},
+	})
+	// when
+	var totalDiscount money.Money
+	for _, discount := range discounts {
+		calculatedDiscount, err := discount.CalculateDiscount(purchaseOrder)
+		assert.Nil(t, err)
+		totalDiscount.Amount += calculatedDiscount.Amount
+	}
+	// then
+	assert.Nil(t, err)
+	assert.True(t, totalDiscount.Amount == float64(0))
+}
+
+func TestCanCalculateADiscountDiscount(t *testing.T) {
+	// given
+	finder := buildDiscountFinder([]models.ComboDiscount{
+		models.NewComboDiscount("001", 3, 200),
+		models.NewComboDiscount("002", 3, 120),
+		models.NewComboDiscount("003", 0, 120),
+	})
+	watchIDs := []models.WatchID{"002", "003", "002", "001", "002"}
+	watchIDsSet := removeDuplicatedIDs(watchIDs)
+	discounts, err := finder.findComboDiscounts(watchIDsSet)
+
+	purchaseOrder := watchIDsToPurchaseOrder(watchIDs, map[models.WatchID]models.WatchCatalogueItem{
+		"001": {ID: "001", Name: "Random1", UnitPrice: 80},
+		"002": {ID: "002", Name: "Random2", UnitPrice: 80},
+		"003": {ID: "003", Name: "Random3", UnitPrice: 50},
+	})
+	// when
+	var totalDiscount money.Money
+	for _, discount := range discounts {
+		calculatedDiscount, err := discount.CalculateDiscount(purchaseOrder)
+		assert.Nil(t, err)
+		totalDiscount.Amount += calculatedDiscount.Amount
+	}
+	// then
+	assert.Nil(t, err)
+	assert.True(t, totalDiscount.Amount == float64(120))
+}
+
+func TestCanCalculateMultipleDiscountDiscount(t *testing.T) {
+	// given
+	finder := buildDiscountFinder([]models.ComboDiscount{
+		models.NewComboDiscount("001", 4, 200),
+		models.NewComboDiscount("002", 3, 120),
+		models.NewComboDiscount("003", 0, 120),
+	})
+	// There are:
+	// 4 watchID "002" => 120 discount
+	// 2 watchID "003  => 0 discount"
+	// 5 watchID "001" => 200 discount
+	watchIDs := []models.WatchID{"002", "002", "003", "003", "002", "001", "002", "001", "001", "001", "001"}
+	watchIDsSet := removeDuplicatedIDs(watchIDs)
+	discounts, err := finder.findComboDiscounts(watchIDsSet)
+
+	purchaseOrder := watchIDsToPurchaseOrder(watchIDs, map[models.WatchID]models.WatchCatalogueItem{
+		"001": {ID: "001", Name: "Random1", UnitPrice: 100},
+		"002": {ID: "002", Name: "Random2", UnitPrice: 80},
+		"003": {ID: "003", Name: "Random3", UnitPrice: 50},
+	})
+	// when
+	var totalDiscount money.Money
+	for _, discount := range discounts {
+		calculatedDiscount, err := discount.CalculateDiscount(purchaseOrder)
+		assert.Nil(t, err)
+		totalDiscount.Amount += calculatedDiscount.Amount
+	}
+	// then
+	assert.Nil(t, err)
+	assert.True(t, totalDiscount.Amount == float64(320))
 }
 
 func buildDiscountFinder(comboDiscounts []models.ComboDiscount) DiscountFinder {
